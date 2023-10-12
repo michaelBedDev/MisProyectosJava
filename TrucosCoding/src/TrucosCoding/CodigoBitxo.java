360a1c5ca26cb908fbf142dd6c27d06c4c559288
Miguel <Miguel@PC_Miguel_2023>
2023-10-12 15:55:05 +0200

    Mi primer commit

---
+++ TrucosCoding/src/TrucosCoding/CodigoBitxo.java
@@ -0,0 +1,14 @@
+package TrucosCoding;
+
+public class CodigoBitxo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Boolean mentira = true;
+		
+		if (mentira != true)
+			System.out.println(mentira);
+	}
+
+}
---
+++ TrucosCoding/src/TrucosCoding/CreandoArrays.java
@@ -0,0 +1,29 @@
+package TrucosCoding;
+
+public class CreandoArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int array [];
+		array = new int [10];
+		
+		int array2 [] = {1,2,3,4,5,6};
+		
+		int matriz1 [][];
+		matriz1 = new int [2][2];
+		
+		//EJEMPLO
+		int matrix[][];
+		matrix = new int[4][4];
+		for (int x=0; x < matrix.length; x++) {
+		  for (int y=0; y < matrix[x].length; y++) {
+		      System.out.print(matrix[x][y]);
+		System.out.println(matriz1);
+		  }
+		}
+				
+		
+	}
+
+}
