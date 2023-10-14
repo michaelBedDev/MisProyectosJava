791bcb84c66409deeb7297f17f4abd1414104947
michaelBedDev <micama2000@gmail.com>
2023-10-14 15:12:09 +0200

    necesidad de instanciar metodos en objetos nuevos

--- RandomRuleProject/src/ruleta/Main.java
+++ RandomRuleProject/src/ruleta/Main.java
@@ -10,7 +10,12 @@ public class Main {
 		
 
 		/*MÉTODO DIBUJAR MENSAJE INICIAL*/
-		rule.dibujoprint.dibujoini(); //Print inicial
+		//Print inicial
+		Dibujo dibujoprint = new Dibujo();
+		dibujoprint.dibujoini();
+		/*ES LO MISMO QUE ------ NECESITO INSTANCIAR UN METODO EN UN OBJETO SIEMPREPARA QUE FUNCIONE NO EN UNA CLASE
+		rule.dibujoprint = dibujoprint;
+		rule.dibujoprint.dibujoini(); */
 
 //AQUI HAY UN LOOP if BANCA > 0. QUIZAS SE DEBERIA INTRODUCIR TODO EL EL LOOP A PARTIR DE AQUI?
 		/*METODO OPCIONES APUESTA*/
