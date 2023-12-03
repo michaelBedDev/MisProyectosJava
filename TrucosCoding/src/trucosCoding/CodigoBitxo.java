03fa822c32ed27346d1cb586dae7a5eae0179922
michaelBedDev <micama2000@gmail.com>
2023-12-03 13:01:52 +0100

    Ordenación apuntes

--- TrucosCoding/src/trucosCoding/CodigoBitxo.java
+++ TrucosCoding/src/trucosCoding/CodigoBitxo.java
@@ -5,7 +5,7 @@ public class CodigoBitxo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Boolean mentira = true;
+		boolean mentira = true;
 		
 		if (mentira != true)
 			System.out.println(mentira);
